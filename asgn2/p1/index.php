31f40322c5b72bdffa2f575cba590b3f7a5a4956
UncleYi <yizhaopu2008@163.com>
2018-03-01 18:52:40 +0800

    wer

--- asgn2/p1/index.php
+++ asgn2/p1/index.php
@@ -9,5 +9,33 @@
     2. The browser type (as one of the following values)
       "Chrome", "Edge", "Firefox", "Safari", "Other"
   */
-echo $_SERVER['HTTP_USER_AGENT'];
+
+  function my_get_browser(){  
+    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 9.0')){  
+        return 'Internet Explorer 9.0';  
+    }  
+    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 8.0')){  
+        return 'Internet Explorer 8.0';  
+    }  
+    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 7.0')){  
+        return 'Internet Explorer 7.0';  
+    }  
+    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 6.0')){  
+        return 'Internet Explorer 6.0';  
+    }  
+    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Firefox')){  
+        return 'Firefox';  
+    }  
+    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Chrome')){  
+        return 'Chrome';  
+    }  
+    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Safari')){  
+        return 'Safari';  
+    }  
+    if(false!==strpos($_SERVER['HTTP_USER_AGENT'],'Opera')){  
+        return 'Opera';  
+    }  
+} 
+
+echo my_get_browser();
 ?>
