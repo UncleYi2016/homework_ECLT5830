4dcac6cd832c36746b5b094c405a5bbaf2882b09
UncleYi <yizhaopu2008@163.com>
2018-03-10 15:27:07 +0800

    sdf

--- asgn3/php/index.php
+++ asgn3/php/index.php
@@ -8,9 +8,14 @@
   $result = $db->query($sql);
   if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-      foreach($row as $element){
-        echo $element . "<br>";
+      foreach($row as $name => $element){
+        if($name != "pass"){
+          echo $element . "<br>";
+        }
       }
+      ?>
+      <img src="../img/<?=$row["user_id"]?>" />
+      <?php
     }
   }
   include('view/index.php');
