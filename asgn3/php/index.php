66ae676f99da118ec118c027893c43587153d8a0
UncleYi <yizhaopu2008@163.com>
2018-03-10 15:14:22 +0800

    d

--- asgn3/php/index.php
+++ asgn3/php/index.php
@@ -4,4 +4,13 @@
   // No special application logic for home page (at least for assignment #3)
   // except to include lib/init.php to initialize some constant
 
+  $sql = "SELECT * FROM `users`";
+  $result = $db->query($sql);
+  if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+      foreach($row as $element){
+        echo $element . "<br>";
+      }
+    }
+  }
   include('view/index.php');
