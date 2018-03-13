1365a411ea3d71229a435dd4f7bb1cb8e81a3892
UncleYi <yizhaopu2008@163.com>
2018-03-13 10:17:29 +0800

    sdf

--- asgn3/php/index.php
+++ asgn3/php/index.php
@@ -20,11 +20,11 @@
     <ul class="user">
     <li id="user_name"><?=$row["first_name"]?> <?=$row["last_name"]?> </li>
     <li id="user_id"><?=$row["user_id"]?></li>
-    <img src="../img/2.jpg" />
     </ul>
     </div>
     <?php
     }
   }
+  <img src="../img/2.jpg" />
   ?>
   </html>
